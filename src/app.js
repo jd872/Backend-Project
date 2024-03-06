6ba7d293527b43913a56e39d56adbee1deb99844
jd872 <jaydpatel87@gmail.com>
2024-03-06 22:20:12 +0530

    Added user route and controller

--- src/app.js
+++ src/app.js
@@ -16,4 +16,12 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 
+// routes import
+
+import userRoutes from './routes/user.routes.js';
+
+// routes declaration
+app.use("/api/v1/users", userRoutes);
+
+
 export default app;
